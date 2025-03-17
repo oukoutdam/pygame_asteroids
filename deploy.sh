d0e8416b40db405164a977595f4d66aeb30ee9b9
Ouk Outdam <outdam.ouk103@gmail.com>
2025-03-17 15:12:17 +0700

    Fix Adding Ignored Files

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-touch ./build/.nojekyll && git add build/ && git commit -m \"Deploy to github pages\" && git subtree push --prefix build origin gh-pages 
+touch ./build/.nojekyll && git add -f build/ && git commit -m "Deploy to github pages" && git subtree push --prefix build origin gh-pages 
